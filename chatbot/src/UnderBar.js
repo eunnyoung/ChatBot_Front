d492fcb214f2e01f2b00792663c98cf3aa90ca5b
haein Kim <ksunperson@naver.com>
2022-01-17 20:39:19 +0900

    bot 기능 추가

--- chatbot/src/UnderBar.js
+++ chatbot/src/UnderBar.js
@@ -1,12 +1,33 @@
 import React, { useState } from "react";
 import { AiOutlineArrowUp } from "react-icons/ai";
 import "./UnderBar.css";
+import swal from "sweetalert";
 
 function UnderBar() {
   // 초기값
   // c언어로 치면, 구조체 요소로 구성된 배열
   const [doChat, setDoChat] = useState("");
-  const [doChats, setDoChats] = useState([]);
+  const [doChats, setDoChats] = useState([
+    <div class="mb-3">
+      <label for="formFile" class="form-label"></label>
+      <input class="form-control" type="file" id="formFile" />
+    </div>,
+    <button>메시지 보내기</button>,
+    <button
+      className="stopChat"
+      onClick={() =>
+        swal("대화를 정말로 종료하시겠습니까?", {
+          dangerMode: true,
+          buttons: true,
+        })
+      }
+    >
+      대화 종료
+    </button>,
+  ]);
+  // const onRemove = () => {
+  //   if(swal.caller=buttons)
+  // }
   const onChange = (event) => setDoChat(event.target.value);
 
   const onSubmit = (event) => {
