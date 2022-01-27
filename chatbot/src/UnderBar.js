4e99f928c1b8dbe430775e447797516384bf19a1
SEO EUNYOUNG <eyoung6u6@daum.net>
2022-01-27 15:05:20 +0900

    객체 리스트 생성

--- chatbot/src/UnderBar.js
+++ chatbot/src/UnderBar.js
@@ -4,30 +4,53 @@ import "./UnderBar.css";
 
 function UnderBar() {
 
-  const [doChat, setDoChat] = useState("");
-  const [doChats, setDoChats] = useState([]);
+  const [message, setMessage] = useState('');
 
-  const [doBot, setDoBot] = useState("");
-  const [doBots, setDoBots] = useState([]);
- 
-  const onChange = (event) => setDoChat(event.target.value);
+ // const { sender , message } = user;
+
+  const [users, setUsers] = useState([
+    {
+      sender:'',
+      message:'',
+      date:null,
+
+    },
+  ]);
+
+  // 시간
+  function getDatetime () {
+    let date = new Date();
+    let time = {
+      hours: date.getHours(),
+      minutes: date.getMinutes(),
+      seconds: date.getSeconds(),
+    };
+    return `${time.hours}:${time.minutes}:${time.seconds}`;
+  }
+
+
+  const onChange = (event) => {
+    console.log(',,,,', message);
+    setMessage(event.target.value);
+  }
 
   const onSubmit = (event) => {
-
     event.preventDefault();
-    
-    if (doChat === "") {
+
+    if (message === "") {
       return;
-    } 
-
-    setDoChats((currentArray) => [...currentArray, doChat]);
-    setDoChat("");
-
-    if (doChat === "안녕") {
-      setDoBots((currentArray) => [...currentArray, "안녕하세요, 반갑습니다."]);
-    } else if (doChat === "이름이 뭐야?") {
-      setDoBots((currentArray) => [...currentArray, "제 이름은 아직 정해지지 않았습니다."]);
     }
+
+    const time = getDatetime();
+    setUsers(users.concat({
+      sender:'user',
+      message:message,
+      date: time,
+    }));
+
+    setMessage(''
+    );
+    
   };
 
   window.scrollBy(0, window.innerHeight);
@@ -35,14 +58,12 @@ function UnderBar() {
   return (
     <>
       <div id="chatBoxWrap">
-        {doChats.map((item, index) => (
+        {users.map((item, index) => (
           <div className="chatUser" key={index}>
-            {item}
-          </div>
-        ))}
-        {doBots.map((item, index) => (
-          <div className="chatBot" key={index}>
-            {item}
+            <button className="onDeleteClick">삭제</button>
+            {console.log("messs", item)}
+            <span className="showTime">{item.date}</span>
+            {item.message}
           </div>
         ))}
       </div>
@@ -53,9 +74,10 @@ function UnderBar() {
           <input
             className="chat"
             type="text"
-            value={doChat}
+            value={message}
             placeholder="채팅을 입력하세요"
             onChange={onChange}
+            onSubmit={onSubmit}
           />
         </form>
         <AiOutlineArrowUp className="btnEnter" onClick={onSubmit} />
@@ -65,3 +87,157 @@ function UnderBar() {
 }
 
 export default UnderBar;
+
+// import React, { useState } from "react";
+// import { AiOutlineArrowUp } from "react-icons/ai";
+// import "./UnderBar.css";
+
+// function UnderBar() {
+//   const [user, setUser] = useState("");
+//   const [users, setUsers] = useState([]);
+
+//   // 시간
+//   const date = new Date();
+//   const time = {
+//     hours: date.getHours(),
+//     minutes: date.getMinutes(),
+//     seconds: date.getSeconds(),
+//   };
+//   const timeString = `${time.hours}:${time.minutes}:${time.seconds}`;
+
+//   const nowTime = (props) => {
+//     return timeString;
+//   }
+
+//   nowTime.props = {
+//     time: timeString,
+//   };
+
+//   const onChange = (event) => setUser(event.target.value);
+
+//   const onSubmit = (event) => {
+//     event.preventDefault();
+
+//     if (user === "") {
+//       return;
+//     }
+
+//     setUsers((currentArray) => [...currentArray, user]);
+//     setUser("");
+
+//     if (user === "안녕") {
+//       setUsers((currentArray) => [...currentArray, "안녕하세요, 반갑습니다."]);
+//     } else if (user === "이름이 뭐야?") {
+//       setUsers((currentArray) => [
+//         ...currentArray,
+//         "제 이름은 아직 정해지지 않았습니다.",
+//       ]);
+//     }
+//   };
+
+//   window.scrollBy(0, window.innerHeight);
+
+//   return (
+//     <>
+//       <div id="chatBoxWrap">
+//         {users.map((item, index) => (
+//           <div className="chatUser" key={index}>
+//             <button className="onDeleteClick">삭제</button>
+//             {<span className="showTime">{timeString}</span>}
+//             {item}
+//           </div>
+//         ))}
+//       </div>
+
+//       {/* 언더바 레이아웃 및 이벤트 등록 */}
+//       <div className="underBar">
+//         <form onSubmit={onSubmit}>
+//           <input
+//             className="chat"
+//             type="text"
+//             value={user}
+//             placeholder="채팅을 입력하세요"
+//             onChange={onChange}
+//           />
+//         </form>
+//         <AiOutlineArrowUp className="btnEnter" onClick={onSubmit} />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default UnderBar;
+
+
+// import React, { useState } from "react";
+// import { AiOutlineArrowUp } from "react-icons/ai";
+// import "./UnderBar.css";
+
+// function UnderBar() {
+//   const [user, setUser] = useState("");
+//   const [users, setUsers] = useState([]);
+
+//   // 시간
+//   const date = new Date();
+//   const time = {
+//     hours: date.getHours(),
+//     minutes: date.getMinutes(),
+//     seconds: date.getSeconds(),
+//   };
+//   const timeString = `${time.hours}:${time.minutes}:${time.seconds}`;
+
+//   const onChange = (event) => setUser(event.target.value);
+
+//   const onSubmit = (event) => {
+//     event.preventDefault();
+
+//     if (user === "") {
+//       return;
+//     }
+
+//     setUsers((currentArray) => [...currentArray, user]);
+//     setUser("");
+
+//     if (user === "안녕") {
+//       setUsers((currentArray) => [...currentArray, "안녕하세요, 반갑습니다."]);
+//     } else if (user === "이름이 뭐야?") {
+//       setUsers((currentArray) => [
+//         ...currentArray,
+//         "제 이름은 아직 정해지지 않았습니다.",
+//       ]);
+//     }
+//   };
+
+//   window.scrollBy(0, window.innerHeight);
+
+//   return (
+//     <>
+//       <div id="chatBoxWrap">
+//         {users.map((item, index) => (
+//           <div className="chatUser" key={index}>
+//             <button className="onDeleteClick">삭제</button>
+//             {<span className="showTime">{timeString}</span>}
+//             {item}
+//           </div>
+//         ))}
+//       </div>
+
+//       {/* 언더바 레이아웃 및 이벤트 등록 */}
+//       <div className="underBar">
+//         <form onSubmit={onSubmit}>
+//           <input
+//             className="chat"
+//             type="text"
+//             value={user}
+//             placeholder="채팅을 입력하세요"
+//             onChange={onChange}
+//           />
+//         </form>
+//         <AiOutlineArrowUp className="btnEnter" onClick={onSubmit} />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default UnderBar;
+
